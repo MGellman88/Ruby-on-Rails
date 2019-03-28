942ee014ea2c29b0b00f2c036bfc3bfafc2b14ac
Mike Gellman <mikegellman88@gmail.com>
2019-03-28 15:15:13 -0500

    added yield and block statement exercises

---
+++ yieldandblock.rb
@@ -0,0 +1,18 @@
+def test
+    yield (5)
+    puts "You are in the method"
+    yield (100)
+end
+test {|i| puts "You are in the block #{i}"}
+
+
+def square(num)
+    puts "num is #{num}"
+
+    x = yield(num)
+    puts "x has a value of #{x}"
+
+    y = yield(num)*x
+    puts "y has a value of #{y}"
+end
+square(5) {|i| i*i}
